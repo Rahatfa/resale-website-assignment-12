84a93cb0df5c07ea071d2a7da39b6f7408e7d8f0
Rahatfa <rhtttt123@gmail.com>
2022-11-26 10:53:12 +0600

    navbar

---
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <Link to='/home' className="btn btn-ghost normal-case text-xl">Mobile <span className='text-red-600 hover:text-sky-400'>World</span> </Link>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal p-0"> 
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/'>Blog</Link></li>
+                    <li><Link to='/'>Log In</Link></li>
+                    </ul>
+                </div>
+        </div>
+        
+    );
+};
+
+export default Navbar;
