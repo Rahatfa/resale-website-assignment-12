a429a0b6a1139df3218e501d0b44d320451a34d6
Rahatfa <rhtttt123@gmail.com>
2022-11-27 20:58:14 +0600

    error page

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Blog from "../../Pages/Blog/Blog";
+import Error from "../../Pages/Error/Error";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 
@@ -25,7 +26,12 @@ const router = createBrowserRouter([
                 element:<Blog></Blog>
             }
         ]
+        
     },
+    {
+        path:'*',
+        element: <Error></Error>
+    }
 ])
 
 
