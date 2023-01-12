c579f7a6ef110291fbe40f152cfd216091956510
Rahatfa <rhtttt123@gmail.com>
2023-01-12 23:40:59 +0600

    final done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import { useEffect, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import Spinner from './Pages/Shared/Spinner/Spinner';
 import router from './Routes/Routes/Routes';
 
-function App() {
+function App() { 
+  const [loading, setLoading] = useState(false);
+  useEffect(()=>{
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    }, 1000)
+  },[])
+
   return (
     <div className='max-w-[1440] mx-auto'>
-      <RouterProvider router={router}></RouterProvider>
-      <Toaster></Toaster>
+      {
+        loading ? <Spinner></Spinner>
+        :
+        <div>
+          <RouterProvider router={router}></RouterProvider>
+          <Toaster></Toaster>
+        </div>
+      }
+      
     </div>
   );
 }
